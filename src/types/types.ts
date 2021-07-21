968026e769d0589f5e9af07ef397ebabbca69a23
tomimarkus991 <tomimarkusalber@gmail.com>
2021-07-21 11:36:08 +0300

    add types

---
+++ src/types/types.ts
@@ -0,0 +1,18 @@
+export type ElementData = {
+  city: string;
+  newTimes: {
+    time1: string;
+    time2: string;
+    time3: string;
+  };
+};
+
+export type Data = {
+  city: string;
+  updatedAt: string;
+  newTimes: {
+    time1: string;
+    time2: string;
+    time3: string;
+  };
+};
